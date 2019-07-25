c5d39d762f62e9f297425125e9ee591245bb1d65
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-25 14:24:54 -0400

    created the three sum generator for zero but still have to make it only give unique answers

---
+++ ThreeSum.java
@@ -0,0 +1,51 @@
+import java.util.Hashtable;
+import java.util.ArrayList;
+
+public class ThreeSum {
+	private static Hashtable<Integer, Integer> hashTable = new Hashtable<>();
+	private static ArrayList<ArrayList<Integer>> arrList = new ArrayList<>();
+	private static void createHash(ArrayList<Integer> intArr) {
+		int index = 0;
+		for (int element : intArr) {
+			hashTable.put(element, index++);
+		}
+	}
+
+	public static ArrayList<ArrayList<Integer>> findMatch(ArrayList<Integer> arr) {
+		createHash(arr);
+		ArrayList<ArrayList<Integer>> arrOfArrays = new ArrayList<>();
+		for (int i = 0; i < arr.size(); i++ ) {
+			for (int j = 0; j< arr.size(); j++) {
+				if (i != j && arr.get(j) <= arr.get(i)) {
+					if (hashTable.contains(-(arr.get(i) + arr.get(j)))) {
+						ArrayList<Integer> tempArr = new ArrayList<>();
+						tempArr.add(arr.get(i));
+						tempArr.add(arr.get(j));
+						tempArr.add(-(arr.get(i) + arr.get(j)));
+						arrOfArrays.add(tempArr);
+					}
+				}
+			}
+		}
+		return arrOfArrays;
+	}
+
+	public static void printNestedArrList(ArrayList<ArrayList<Integer>> arr) {
+		for (ArrayList<Integer> list : arr) {
+			String strArr = "";
+			for (int element : list) {
+				strArr += element + " ";
+			}
+			System.out.println(strArr);
+		}
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		for (int i =0; i < args.length; ++i) {
+			arr.add(Integer.parseInt(args[i]));
+		}
+		printNestedArrList(findMatch(arr));
+	}
+}
