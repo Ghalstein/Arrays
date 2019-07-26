3262231fc2448ce10426e7112a9b3f61ec2cfd25
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-26 08:04:07 -0400

    finished up the basic layout now time to functionalize it

---
+++ RomanToDecimal.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class RomanToDecimal {
+
+	// hashmap key for detemrining the values of each numeral
+	private static HashMap<Character, Integer> map = new HashMap<>();
+
+	// function that adds the elements to the hash
+	private static void createHash() {
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+	}
+
+	//converts the string of roman numerals to integers
+	public static int convert(String romanNumerals) {
+		// adds elements to the hash
+		createHash();
+		int value = 0;
+		for (int i = 0; i < romanNumerals.length(); i++) {
+			if (romanNumerals.charAt(i) < romanNumerals.charAt(i + 1) ) {
+				// exception
+			}
+			else {
+				// add as normal
+			}
+		}
+		return value;
+	}
+
+	public static void main(String args[]) {
+
+		System.out.println(convert(args[0]));
+	}
+}
