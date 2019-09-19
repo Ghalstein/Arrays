60f5923905518a522b67d174f5bffa46f5f8c44f
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-19 15:59:00 -0400

    planning the swaps

--- RotateMatrix.java
+++ RotateMatrix.java
@@ -1,7 +1,13 @@
+/*
+First want to start with the outer index and swap to form a rotation
+In order to do this would have to swap all of the first in eahc side then 
+keep doing it until the end of the row is reached
+*/
+
 public class RotateMatrix {
 
 
 	public static void main(String[] args) {
-		
+
 	}
 }
