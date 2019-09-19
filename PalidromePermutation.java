6dc8500b8df96e1eaa6cd1e389acdac32c7a27cb
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-19 01:23:57 -0400

    unit tested

--- PalidromePermutation.java
+++ PalidromePermutation.java
@@ -1,6 +1,34 @@
+import java.util.*;
+
 public class PalidromePermutation {
 
+	public static boolean valid(String str) {
+		char[] chars = str.toCharArray();
+		Arrays.sort(chars);
+		boolean oddFound = false;
+		int repeats = 1;
+		for (int i = 0; i < chars.length - 1; ++i) {
+			if (chars[i] == chars[i + 1]) {
+				++repeats;
+			}
+			else {
+				if (repeats % 2 == 1) {
+					if (oddFound) {
+						return false;
+					}
+					else {
+						oddFound = true;
+					}
+				}
+				repeats = 1;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
-		
+		System.out.println(valid("racecar"));
+		System.out.println(valid("tacocat"));
+		System.out.println(valid("tea"));
 	}
 }
