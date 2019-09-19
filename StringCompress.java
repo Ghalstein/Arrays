b3feba3b7e5777acda9ed433acf803dd411f3674
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-19 15:11:38 -0400

    created the hashing string method

--- StringCompress.java
+++ StringCompress.java
@@ -4,9 +4,29 @@ Will use a hashmap to map out the repeats and increment for each repeat
 Then will iterate trhoguh the hashmap to create the new compressed string
 */
 
+import java.util.*;
+
 public class StringCompress {
 
-	
+
+	// creates a hashmap that tallies the repeats and appearence  
+	private static HashMap<Character, Integer> hashString(String str) {
+		HashMap<Character, Integer> hashMap = new HashMap<>();
+		for (int i = 0; i < str.length(); ++i) {
+			// if the key already appeared just increment the value else create it
+			if (hashMap.containsKey(str.charAt(i))) {
+				hashMap.put(str.charAt(i), hashMap.get(str.charAt(i)) + 1);
+			}
+			else {
+				hashMap.put(str.charAt(i), 1);
+			}
+		}
+		return hashMap;
+	}
+
+	public static String create() {
+		
+	}
 
 	public static void main(String[] args) {
 
