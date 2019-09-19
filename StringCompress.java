68fb115c497dd29568481df9bddeb1c83a343821
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-19 15:00:51 -0400

    made the blurb for the program

--- StringCompress.java
+++ StringCompress.java
@@ -1,3 +1,9 @@
+/*
+Program that compresses the repeats in a string to the character followed by the number of times
+Will use a hashmap to map out the repeats and increment for each repeat 
+Then will iterate trhoguh the hashmap to create the new compressed string
+*/
+
 public class StringCompress {
 
 	
