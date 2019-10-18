72c240a756f34c471c1c6b23ea10fca7b874e35b
Garrett Halstein <garrett.halstein@emory.edu>
2019-10-17 23:47:26 -0400

    method created for FindSubstring

--- FindSubstring.java
+++ FindSubstring.java
@@ -1,6 +1,30 @@
 public class FindSubstring {
 
-  public static void main(String[] args) {
+  public int strStr(String haystack, String needle) {
+    // have a string and substring to search for linearly within the string
+    // iterator for substring
+    int j = 0;
+    for (int i = 0; i < haystack.length(); ++i) {
+      
+      if (needle.equals("")) return 0;
+      if (needle.length() > haystack.length()) return -1;
+      if (haystack.charAt(i) == needle.charAt(j)) {
+        System.out.println(j);
+        ++j;
+        if (j == needle.length()) return ++i - needle.length();
+      }
+      else {
+        // acounts for any patterns overshadowing the correct answer
+        if (j != 0) i -= j;
+        j = 0; 
+      }
+    }
     
+    if (j == needle.length()) return haystack.length() - needle.length();
+    
+    return -1;
+  }
+  public static void main(String[] args) {
+
   }
 }
